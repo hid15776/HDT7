4dbbc000db1dfc4c19ff65612848cc3e4ec9ac5f
HiramRV <rub15044@uvg.edu.gt>
2016-09-26 19:16:28 -0600

    Nuevo método

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,115 +1,185 @@
-import java.lang.*;
+public class BinaryTree {
 
-public class BinaryTree<E> {
-	private E val; // value associated with node 
-	private BinaryTree<E> parent; // parent of node 
-	private BinaryTree<E> left, right; // children of nod
-	
-	public BinaryTree(){
-		val = null; 
-		parent = null; 
-		left = right = this;
+	Node root;
+
+	public void addNode(int key, String name) {
+
+		// Create a new Node and initialize it
+
+		Node newNode = new Node(key, name);
+
+		// If there is no root this becomes root
+
+		if (root == null) {
+
+			root = newNode;
+
+		} else {
+
+			// Set root as the Node we will start
+			// with as we traverse the tree
+
+			Node focusNode = root;
+
+			// Future parent for our new Node
+
+			Node parent;
+
+			while (true) {
+
+				// root is the top parent so we start
+				// there
+
+				parent = focusNode;
+
+				// Check if the new node should go on
+				// the left side of the parent node
+
+				if (key < focusNode.key) {
+
+					// Switch focus to the left child
+
+					focusNode = focusNode.leftChild;
+
+					// If the left child has no children
+
+					if (focusNode == null) {
+
+						// then place the new node on the left of it
+
+						parent.leftChild = newNode;
+						return; // All Done
+
+					}
+
+				} else { // If we get here put the node on the right
+
+					focusNode = focusNode.rightChild;
+
+					// If the right child has no children
+
+					if (focusNode == null) {
+
+						// then place the new node on the right of it
+
+						parent.rightChild = newNode;
+						return; // All Done
+
+					}
+
+				}
+
+			}
+		}
 
 	}
-	// post: constructor that generates an empty node
+
 	
-	public BinaryTree(E value){
-		val = value; 
-		right = left = new BinaryTree<E>(); 
-		setLeft(left); 
-		setRight(right);
-		
-	}
-	// post: returns a tree referencing value and two empty subtrees
-	
-	public BinaryTree(E value, BinaryTree<E> left, BinaryTree<E> right){
-		val = value; 
-		if (left == null) { 
-			left = new BinaryTree<E>(); 
-			} 
-		setLeft(left); 
-		if (right == null) { 
-			right = new BinaryTree<E>(); 
-			} 
-		setRight(right);
-	} 
-	// post: returns a tree referencing value and two subtrees
-	
-	public BinaryTree<E> left() {
-		// post: returns reference to (possibly empty) left subtree
-		return left;
-	}
-	
-	public BinaryTree<E> right() {
-		// post: returns reference to (possibly empty) left subtree
-		return right;
-	}
-	
-	public BinaryTree<E> parent(){
-	}
-	// post: returns reference to parent node, or null
-	
-	public void setLeft(BinaryTree<E> newLeft){
-		// post: sets left subtree to newLeft // re-parents newLeft if not null
-		
-		if (isEmpty()) 
-			return; 
-		if (left != null && left.parent() == this) left.setParent(null);
-		left = newLeft; 
-		left.setParent(this);
-	} 
-	
-	public void setRight(BinaryTree<E> newRight){
-		// post: sets Right subtree to newLeft // re-parents newRight if not null		
-		if (isEmpty()) 
-			return; 
-		if (right != null && right.parent() == this) right.setParent(null);
-		right = newRight; 
-		right.setParent(this);
-	} 
-	
-	protected void setParent(BinaryTree<E> newParent) {
-	// post: re-parents this node to parent reference, or null
-		if (!isEmpty()) { 
-			parent = newParent; }
-	}
-	
-	public Iterator<E> iterator() {
-		// post: returns an in-order iterator of the elements
-		
-	}
-	
-	public boolean isLeftChild() {
-		// post: returns true if this is a left child of parent
-		if(this == parent.left()){
-			return true;
-		}else{
-			return false;
+	// All nodes are visited in ascending order
+	// Recursion is used to go to one node and
+	// then go to its child nodes and so forth
+
+	public void inOrderTraverseTree(Node focusNode) {
+
+		if (focusNode != null) {
+
+			// Traverse the left node
+
+			inOrderTraverseTree(focusNode.leftChild);
+
+			// Visit the currently focused on node
+
+			System.out.println(focusNode);
+
+			// Traverse the right node
+
+			inOrderTraverseTree(focusNode.rightChild);
+
 		}
+
 	}
-	
-	public boolean isRightChild() {
-		// post: returns true if this is a left child of parent
-		if(this == parent.right()){
-			return true;
-		}else{
-			return false;
+
+	public String findName(String key) {
+		Node focusNode = root;
+		while (!Integer.toString(focusNode.key).equalsIgnoreCase(key)) {
+			if (key.compareTo(Integer.toString(focusNode.key)) < 0) {
+				focusNode = focusNode.leftChild;
+			} else {
+				focusNode = focusNode.rightChild;
+			}
+			if (focusNode == null)
+				return null;
 		}
+		return focusNode.name.toString();
+
 	}
+
+
 	
-	public E value(){
-		// post: returns value associated with this node
-		return val;
-	} 
-	
-	public void setValue(E value) {
-	// post: sets the value associated with this node
-		val = value;
+	public void preorderTraverseTree(Node focusNode) {
+
+		if (focusNode != null) {
+
+			System.out.println(focusNode);
+
+			preorderTraverseTree(focusNode.leftChild);
+			preorderTraverseTree(focusNode.rightChild);
+
+		}
+
 	}
-	
-	
-	//PUSE ESTO AQUI
-	public String toString() {
-		return name + " has the key " + key;
+
+	public void postOrderTraverseTree(Node focusNode) {
+
+		if (focusNode != null) {
+
+			postOrderTraverseTree(focusNode.leftChild);
+			postOrderTraverseTree(focusNode.rightChild);
+
+			System.out.println(focusNode);
+
+		}
+
 	}
+
+	public Node findNode(int key) {
+
+		// Start at the top of the tree
+
+		Node focusNode = root;
+
+		// While we haven't found the Node
+		// keep looking
+
+		while (focusNode.key != key) {
+
+			// If we should search to the left
+
+			if (key < focusNode.key) {
+
+				// Shift the focus Node to the left child
+
+				focusNode = focusNode.leftChild;
+
+			} else {
+
+				// Shift the focus Node to the right child
+
+				focusNode = focusNode.rightChild;
+
+			}
+
+			// The node wasn't found
+
+			if (focusNode == null)
+				return null;
+
+		}
+
+		return focusNode;
+
+	}
+
+
 }
+
